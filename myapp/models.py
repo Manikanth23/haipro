294e9134a97359d84ce280e7c7b9d9f64d80da13
Manikanth23 <bongonimanikanth@gmail.com>
2022-04-26 10:50:04 +0530

    paragraph added

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,12 @@
+import email
 from django.db import models
 
 # Create your models here.
+class Data(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField(max_length=100)
+    img=models.ImageField()
+    phoneno=models.IntegerField()
+
+    def __str__(self):
+        return self.name
